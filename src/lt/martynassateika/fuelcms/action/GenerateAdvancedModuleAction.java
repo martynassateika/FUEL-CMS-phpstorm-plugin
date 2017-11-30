6221b38b8daa9510dfccb9bfd8fbd4c3dcf742b2
Martynas Sateika <martynas.sateika@gmail.com>
2017-11-30 01:50:22 +0000

    Add advanced module generation functionality

---
+++ src/lt/martynassateika/fuelcms/action/GenerateAdvancedModuleAction.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright 2017 Martynas Sateika
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package lt.martynassateika.fuelcms.action;
+
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.ui.DialogWrapper;
+import lt.martynassateika.fuelcms.ui.GenerateAdvancedModuleDialogWrapper;
+import lt.martynassateika.fuelcms.ui.GenerateSimpleModuleDialogWrapper;
+import lt.martynassateika.fuelcms.util.FuelCmsProjectUtils;
+
+/**
+ * Opens up a modal window for simple module creation.
+ */
+public class GenerateAdvancedModuleAction extends AnAction {
+
+    @Override
+    public void actionPerformed(AnActionEvent anActionEvent) {
+        DialogWrapper dialog = new GenerateAdvancedModuleDialogWrapper(anActionEvent.getProject());
+        dialog.show();
+    }
+
+    @Override
+    public void update(AnActionEvent e) {
+        boolean isFuelCmsProject = FuelCmsProjectUtils.isFuelCmsProject(e.getProject());
+        e.getPresentation().setEnabled(isFuelCmsProject);
+    }
+
+}
---
+++ src/lt/martynassateika/fuelcms/generate/FuelCmsAdvancedModule.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright 2017 Martynas Sateika
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package lt.martynassateika.fuelcms.generate;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Represents a FuelCMS advanced module class.
+ */
+public class FuelCmsAdvancedModule implements GenerateTarget {
+
+    private final String name;
+
+    /**
+     * @param name advanced module name
+     */
+    public FuelCmsAdvancedModule(@NotNull String name) {
+        this.name = name;
+    }
+
+    @Override
+    public List<String> getCommand() {
+        List<String> strings = new ArrayList<>(3);
+        strings.add("index.php");
+        strings.add("fuel/generate/advanced");
+        strings.add(name);
+        return strings;
+    }
+
+}
